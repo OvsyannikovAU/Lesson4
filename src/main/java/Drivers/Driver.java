7386b54cdfdc4ed3683b5c9bc648b97d9373b731
Igor <rtviwe@gmail.com>
2021-09-24 13:38:41 +0300

    Update Driver.java

--- src/main/java/Drivers/Driver.java
+++ src/main/java/Drivers/Driver.java
@@ -2,6 +2,7 @@ package Drivers;
 
 public interface Driver {
 
+    // TODO: по кодстайлу жавы методы с маленькой буквы называют, voice например
     public String Voice();
     public String Move();
     public String Footprint();
